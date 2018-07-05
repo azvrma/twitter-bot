ba58a0235c98104a65891564a857b781cc2301ae
Anuj Verma <7azvrma@gmail.com>
2018-07-05 09:22:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ console.log("AutoDM App started successfully 🙌🙌");
 
 AutoDM();
 
-//const AutoTweet = require("./src/AutoTweet.js");
+const AutoTweet = require("./src/AutoTweet.js");
 
-//console.log("AutoTweet App started successfully 🙌🙌");
+console.log("AutoTweet App started successfully 🙌🙌");
